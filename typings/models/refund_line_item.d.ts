5198e4780515a4ba1f41dfde8c7466e12ee20afb
Danijel <dan.baternik@gmail.com>
2021-02-12 21:27:29 +1000

    New release

--- typings/models/refund_line_item.d.ts
+++ typings/models/refund_line_item.d.ts
@@ -2,7 +2,7 @@ import { ShopifyObject } from "./base";
 import { LineItem } from "./line_item";
 
 export interface RefundLineItem extends ShopifyObject {
-    line_item_id?: string;
+    line_item_id?: number;
     line_item?: LineItem;
     quantity?: number;
     restock_type?: string;
