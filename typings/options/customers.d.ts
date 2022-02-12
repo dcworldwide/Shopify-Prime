76235e479f97ccb86e57cbe53c784d9b69419e6e
Danijel <dcworldwide@users.noreply.github.com>
2022-02-12 13:48:32 +1000

    New release

--- typings/options/customers.d.ts
+++ typings/options/customers.d.ts
@@ -13,6 +13,13 @@ export interface CustomerSearchOptions extends FieldOptions, ListOptions {
      * Set the field and direction by which to order results. default: last_order_date DESC
      */
     order: string
+
+    /**
+     * Text to search for in the shop's customer data.
+     * 
+     * Supported queries: accepts_marketing, activation_date, address1, address2, city, company, country, customer_date, customer_first_name, customer_id, customer_last_name, customer_tag, email, email_marketing_state, first_name, first_order_date, id, last_abandoned_order_date, last_name, multipass_identifier, orders_count, order_date, phone, province, shop_id, state, tag, total_spent, updated_at, verified_email, product_subscriber_status
+     */
+    query?: string
 }
 
 export interface CustomerSavedSearchListOptions extends FieldOptions, ListOptions {
