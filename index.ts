a2f5f26c42d93bd0d7b37bbe6adddd41bbc7ae3f
dcworldwide <dcworldwide@users.noreply.github.com>
2025-01-28 15:18:50 +1000

    New release

--- index.ts
+++ index.ts
@@ -1,30 +1,26 @@
-import * as Infrastructure from "./infrastructure";
-import ApplicationCredits from "./services/application_credits";
-import Articles from "./services/articles";
-import * as Auth from "./services/auth";
-import Blogs from "./services/blogs";
-import Charges from "./services/charges";
-import Countries from "./services/countries";
-import Customers from "./services/customers";
-import CustomerSavedSearches from "./services/customer_saved_search";
-import CustomCollections from "./services/custom_collections";
-import Discounts from "./services/discounts";
-import GiftCards from "./services/gift_cards";
-import InventoryItems from "./services/inventory_items";
-import Orders from "./services/orders";
-import Pages from "./services/pages";
-import PriceRules from "./services/price_rules";
-import PriceRuleDiscounts from "./services/price_rule_discounts";
-import Products from "./services/products";
-import RecurringCharges from "./services/recurring_charges";
-import Redirects from "./services/redirects";
-import ScriptTags from "./services/script_tags";
-import Shops from "./services/shops";
-import SmartCollections from "./services/smart_collections";
-import UsageCharges from "./services/usage_charges";
-import Webhooks from "./services/webhooks";
+import * as Infrastructure from "./infrastructure"
+import ApplicationCredits from "./services/application_credits"
+import Articles from "./services/articles"
+import * as Auth from "./services/auth"
+import Blogs from "./services/blogs"
+import Charges from "./services/charges"
+import Countries from "./services/countries"
+import CustomerSavedSearches from "./services/customer_saved_search"
+import Customers from "./services/customers"
+import Discounts from "./services/discounts"
+import GiftCards from "./services/gift_cards"
+import InventoryItems from "./services/inventory_items"
+import Orders from "./services/orders"
+import Pages from "./services/pages"
+import PriceRuleDiscounts from "./services/price_rule_discounts"
+import PriceRules from "./services/price_rules"
+import RecurringCharges from "./services/recurring_charges"
+import Redirects from "./services/redirects"
+import ScriptTags from "./services/script_tags"
+import Shops from "./services/shops"
+import UsageCharges from "./services/usage_charges"
+import Webhooks from "./services/webhooks"
 
-export { Infrastructure };
-export { Auth, Shops, Blogs, Customers, CustomerSavedSearches, Countries, Orders, Products, InventoryItems, Charges, Articles, Webhooks, ScriptTags, UsageCharges, RecurringCharges, ApplicationCredits, Discounts, GiftCards, Redirects, PriceRules, PriceRuleDiscounts, CustomCollections, SmartCollections, Pages };
+export { ApplicationCredits, Articles, Auth, Blogs, Charges, Countries, Customers, CustomerSavedSearches, Discounts, GiftCards, Infrastructure, InventoryItems, Orders, Pages, PriceRuleDiscounts, PriceRules, RecurringCharges, Redirects, ScriptTags, Shops, UsageCharges, Webhooks }
 
 
